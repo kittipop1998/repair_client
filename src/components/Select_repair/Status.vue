fb92622c73e77985c840fca8138897e6dbdd355e
Wine Kittipop <37920511+kittipop1998@users.noreply.github.com>
2020-10-23 19:37:43 +0700

    date

--- src/components/Select_repair/Status.vue
+++ src/components/Select_repair/Status.vue
@@ -1,10 +1,10 @@
 <template>
-    
+    $END$
 </template>
 
 <script>
     export default {
-        name: "Status_O"
+        name: "Status"
     }
 </script>
 
--- src/views/manager/Status/Edit_M.vue
+++ src/views/manager/Status/Edit_M.vue
@@ -1,10 +1,10 @@
 <template>
-    
+    $END$
 </template>
 
 <script>
     export default {
-        name: "Technician_O"
+        name: "Edit_M"
     }
 </script>
 
